b5fef4e774ab00a9552f61cfafc9b2c37e5890ca
Paulo Sebben <59052216+opaulosebben@users.noreply.github.com>
2022-03-06 21:09:18 -0300

    final commit

--- include/calculadora.h
+++ include/calculadora.h
@@ -4,31 +4,30 @@
 
 class calculadora
 {
-    private:
-        int operando1;
-        int operando2;
+private:
+    float operando1;
+    float operando2;
+    float resultado;
+
+public:
+
+    calculadora();
+    calculadora(float operando1, float operando2);
+
+    void soma();
+    void subtrai();
+    void multiplica();
+    void divide();
+
+    float getOperando1();
+    float getOperando2();
+    float getResultado();
 
 
-
-    public:
-
-        void soma(int);
-        void subtrai (int);
-        void multiplica (int);
-        void divide (int);
-
-        float setOperando1(float);
-        float setOperando2(float);
-
-        float getResultado();
+    void setOperando1(float op1);
+    void setOperando2(float op2);
 };
 
 #endif // CALCULADORA_H
 
-/*Um método chamado setOperando1 que deverá receber como parâmetro um valor float e não retorna valores
-Um método chamado setOperando2 que deverá receber como parâmetro um valor float e não retorna valores
-Um método chamado soma que não recebe  e não retorna valores
-Um método chamado subtrai que não recebe  e não retorna valores
-Um método chamado multiplica que não recebe  e não retorna valores
-Um método chamado divide que não recebe  e não retorna valores
-Um método chamado getResultado que não recebe parâmetros e retorna um valor float.*/
+
